e26ee26168abbffa960a27121894912a8932f4de
Paula-Wang <nimopaula8@gmail.com>
2026-02-16 12:15:47 +0300

    add redis

---
+++ src/captioning_lib/redis_client.py
@@ -0,0 +1,12 @@
+import redis
+
+
+class RedisClient:
+    def _init_(self):
+        self.client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+
+    def rpush(self, queue_name, data):
+        return self.client.rpush(queue_name, data)
+
+    def lpop(self, queue_name):
+        return self.client.lpop(queue_name)
